c1e21c9ddca5eafe284b5a35231e622a399cd998
Juno <santosjuno@gmail.com>
2016-02-25 18:13:21 +0400

    still working on that dynamic grid

--- JS101/JSSketchPad/script.js
+++ JS101/JSSketchPad/script.js
@@ -1,4 +1,27 @@
-$(document).ready(function() {
-  //adding divs on the HTML
-  $(".container").append('<ul id="row1">')
+
+$(document).ready(function () {
+
+  var nor = prompt("input number of rows");
+    //NaN validation
+    /*if (nor == NaN) {
+      alert("NaN");
+      nor = prompt("input a number!")
+    }*/
+
+  var noc = prompt("input number of columns");
+
+  var divsR = '<li><div class="block></div></li>"';
+  var rows = divsR.repeat(nor);
+
+  var divsC = '<ul>' + rows + '</ul>';
+  var columns = divsC.repeat(noc);
+
+  $(".container").append(noc)
+
+
+
+
+
+
+
 })
