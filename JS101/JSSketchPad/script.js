044c79b6cc7f68864ae8018e8a8d358889d5cea7
Juno <santosjuno@gmail.com>
2016-02-28 14:40:27 +0400

    code is working but lacks polish

--- JS101/JSSketchPad/script.js
+++ JS101/JSSketchPad/script.js
@@ -2,44 +2,25 @@
 $(document).ready(function () {
 
   //need to reverse cols and rows because of inline-block formatting
-  var noc = 5;
-  var nor = 4;
-
-  $("#addRow").click(function() {
-    nor++;
-    $("#nor").html(nor);
-  })
-
-  $("#minRow").click(function() {
-    nor--;
-    $("#nor").html(nor);
-  })
-
-  $("#addCol").click(function() {
-    noc++;
-    $("#noc").html(noc);
-  })
-
-  $("#minCol").click(function() {
-    noc--;
-    $("#noc").html(noc);
-  })
-
+  var noc = prompt("enter number of colums")
+  var nor = prompt("enter number of rows")
 
   //need to fix string concatenation for proper number of elements in grid
   var colTemp = '<li><div class="block"></div></li>';
   var divsC = colTemp.repeat(noc);
 
   var rowTemp = '<ul>' + divsC + '</ul><br>';
-  var divsR = '"' + rowTemp.repeat(nor) + '"';
+  var divsR =  rowTemp.repeat(nor);
 
-  $("#submit").click(function() {
-    $(".container").empty();
-    $(".container").append(divsC);
-  })
+  $(".container").append(function(){
+    $(".container").append(divsR);
+  });
 
-  $("#reset").click(function () {
-    $(".container").empty()
-  })
-
-})
+  //to change colors on hover
+  $(".block").hover(function() {
+    $(this).removeclass();
+    $(this).addClass("block-hover");
+  }, function () {
+    $(this).addClass("block-hover");
+  });
+});
